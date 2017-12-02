73f1bdd5db45017e195271611eeee42b3453d6e3
Wojciech <wojciech.niemiec.pv@gmail.com>
2017-12-02 18:44:06 +0100

    Window.fxml added

--- src/main/java/com/wniemiec/cheat/neural/BipolarSigmoidalFunction.java
+++ src/main/java/com/wniemiec/cheat/neural/BipolarSigmoidalFunction.java
@@ -2,30 +2,11 @@ package com.wniemiec.cheat.neural;
 
 public class BipolarSigmoidalFunction implements ActivationFunction {
 
-    /**
-     * Learning speed
-     */
-//    private double beta;
-
-    public BipolarSigmoidalFunction() {
-//        beta = 0.1;
-    }
-
-//    public BipolarSigmoidalFunction(double beta) {
-//        if (isValid(beta)) {
-//            this.beta = beta;
-//        }
-//    }
-
     @Override
     public final Double apply(Double x) {
         return  (1 - Math.pow(Math.E, -x)) /
                 (1 + Math.pow(Math.E, -x));
     }
-
-//    private boolean isValid(double beta) {
-//        return beta > 0 && beta <= 1;
-//    }
 
     @Override
     public Double applyDerivative(Double x) {
